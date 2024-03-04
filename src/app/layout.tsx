01561e1b09d2a02e99cee851f786f428e2f43116
JesseyStend <thejes@hotmail.nl>
2024-03-04 16:21:42 +0100

    added init state for website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="fixed items-center flex bg-primary min-h-24 w-full shadow-lg z-10 px-8 ">
+          <h3 className="flex-auto">Sarah&apos;s oldies</h3>
+          <div className="flex-none grid grid-cols-5 gap-4 items-center">
+            <Link href="#OverMij" className=" text-xl font-bold">
+              Over mij
+            </Link>
+            <Link href="#MijnAuto" className=" text-xl font-bold">
+              Mijn auto
+            </Link>
+            <Link
+              href="#Contact"
+              className=" text-xl font-bold bg-background rounded-xl border-2 py-2 px-8 shadow-md col-start-4 col-end-6"
+            >
+              Neem contact
+            </Link>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      sm: "0.750rem",
+      base: "1rem",
+      xl: "1.333rem",
+      "2xl": "1.777rem",
+      "3xl": "2.369rem",
+      "4xl": "3.158rem",
+      "5xl": "4.210rem",
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "700",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        text: "var(--text)",
+        background: "var(--background)",
+        primary: "var(--primary)",
+        accent: "var(--accent)",
       },
     },
   },
